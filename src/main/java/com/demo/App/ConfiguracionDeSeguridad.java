e6444cf4504faed96ee91eade394ab849eb2e260
Renato Servadei <91791046+Renato-Servadei@users.noreply.github.com>
2022-06-06 13:56:23 -0300

    Update ConfiguracionDeSeguridad.java

--- src/main/java/com/demo/App/ConfiguracionDeSeguridad.java
+++ src/main/java/com/demo/App/ConfiguracionDeSeguridad.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import com.demo.App.repository.UserRepository;
-import com.demo.App.security.jwt.JwtTokenFilter;
+import com.demo.App.seguridad.jwt.jwtTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -25,7 +25,7 @@ public class ConfiguracionDeSeguridad extends WebSecurityConfigurerAdapter {
     private UserRepository userRepo;
     
     @Autowired
-    private JwtTokenFilter jwtTokenFilter;
+    private jwtTokenFilter jwtTokenFilter;
     
     @Bean
     PasswordEncoder passwordEncoder() {
